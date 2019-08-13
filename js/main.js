8c9f357bc099e14df00b5a48954c0dd690ec4972
Alexey Merkulov <bgates@inbox.ru>
2019-08-13 22:28:57 +0300

    ДОбавлен блок Спецпредложения и Генплан

--- js/main.js
+++ js/main.js
@@ -6,6 +6,33 @@ $(function (){
     $(".about__hidden").removeClass("about__hidden")
   })
 
+  $(".offer__items").slick({
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+    arrows: true,
+    prevArrow: '<div class="offer__left"></div>',
+    nextArrow: '<div class="offer__right"></div>',
+  });
 
 
 })
