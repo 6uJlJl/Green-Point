7a569814d7576c217844b71aa7e303731e3712e9
Alexey Merkulov <bgates@inbox.ru>
2019-08-13 19:43:03 +0300

    Сверстан блок Инфраструктура

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function (){
+
+  $(".about__more").click(function (event) {
+    event.preventDefault();
+    $(".about__more").hide();
+    $(".about__hidden").removeClass("about__hidden")
+  })
+
+
+
+})
